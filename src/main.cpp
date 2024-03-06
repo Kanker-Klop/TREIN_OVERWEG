7b5f5b735ccd5b78db526a5f4d99f82d43027025
Kanker-Klop <150671397+Kanker-Klop@users.noreply.github.com>
2024-03-06 12:36:22 +0100

    De code; ongeoptimaliseerd
    Nu nog efkes optimaliseren

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,54 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
+#include <Servo.h>
+#define aankomstknop 2
+#define vertrekknop 3
+#define witlicht 4
+#define rood1 5
+#define rood2 6
+Servo slagboom;
+bool wit = 1;
+bool rood = 0;
+bool aankomstpiet = 0;
+bool vertrekpiet = 0;
+bool treiniser = 0;
+void aankomst() {
+  digitalWrite(witlicht, LOW);
+  digitalWrite(rood1, rood);
+  digitalWrite(rood2, !rood);
+  slagboom.write(90);
+  treiniser = 1;
+  return;
+}
+void vertrek() {
+  slagboom.write(0);
+  digitalWrite(rood1, LOW);
+  digitalWrite(rood2, LOW);
+  treiniser = 0;
+}
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  slagboom.attach(9);
+  attachInterrupt(digitalPinToInterrupt(2), aankomst, RISING);
+  attachInterrupt(digitalPinToInterrupt(3), vertrek, RISING);
+  slagboom.write(0);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  while ((vertrekpiet == 0) && treiniser) {
+    rood = !rood;
+    digitalWrite(rood1, rood);
+    digitalWrite(rood2, !rood);
+    delay(175);
+    digitalWrite(rood1, !rood);
+    digitalWrite(rood2, rood);
+    delay(175);
+  }
+  digitalWrite(rood1, LOW);
+  digitalWrite(rood2, LOW);
+  digitalWrite(witlicht, wit);
+  delay(750);
+  wit = !wit;
 }
