d79e37396e90f88ffee7d5f7663ade4b2fc4b7f9
Kanker-Klop <150671397+Kanker-Klop@users.noreply.github.com>
2024-03-13 12:21:19 +0100

    Verder geoptimaliseerd
    Geen idee hoe ik dit niet eerder had gezien.

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,6 @@
 Servo slagboom;
 bool wit = 1;
 bool rood = 0;
-bool aankomstpiet = 0;
-bool vertrekpiet = 0;
 bool treiniser = 0;
 void aankomst() {
   digitalWrite(4, LOW);
@@ -32,7 +30,7 @@ void setup() {
   slagboom.write(0);
 }
 void loop() {
-  while ((vertrekpiet == 0) && treiniser) {
+  while (treiniser) {
     rood = !rood;
     digitalWrite(5, rood);
     digitalWrite(6, !rood);
